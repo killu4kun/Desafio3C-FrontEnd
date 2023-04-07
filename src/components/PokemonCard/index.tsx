4f1c4b47b671d1adf220ebda4c37113dc754ca3b
matheus.soares2 <matheus.soares@finderup.com.br>
2023-04-07 20:49:14 -0300

    feat: more stuff

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -1,21 +1,7 @@
-import { PokemonProps } from '../../store';
-
-import {
-  Box,
-  Heading,
-  Text,
-  Img,
-  Flex,
-  Center,
-  useColorModeValue,
-  HStack,
-} from '@chakra-ui/react';
+import { Box, Text, Flex, Center, HStack } from '@chakra-ui/react';
 import { BsArrowUpRight, BsHeartFill, BsHeart } from 'react-icons/bs';
-import { useState } from 'react';
 
-function PokemonCard({ pokemon }: any) {
-  const [liked, setLiked] = useState(false);
-
+function PokemonCard({ pokemon, liked, setLiked }: any) {
   return (
     <Center py={6}>
       <Box
@@ -27,20 +13,7 @@ function PokemonCard({ pokemon }: any) {
         bg="white"
         border={'1px'}
         borderColor="black"
-        boxShadow={useColorModeValue('6px 6px 0 black', '6px 6px 0 cyan')}
       >
-        <Box h={'200px'} borderBottom={'1px'} borderColor="black">
-          <Img
-            src={
-              'https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
-            }
-            roundedTop={'sm'}
-            objectFit="cover"
-            h="full"
-            w="full"
-            alt={'Blog Image'}
-          />
-        </Box>
         <Box p={4}>
           <Box
             bg="black"
@@ -50,17 +23,10 @@ function PokemonCard({ pokemon }: any) {
             color="white"
             mb={2}
           >
-            <Text fontSize={'xs'} fontWeight="medium">
+            <Text fontSize={'lg'} fontWeight="medium">
               {pokemon.name}
             </Text>
           </Box>
-          <Heading color={'black'} fontSize={'2xl'} noOfLines={1}>
-            React v18.0
-          </Heading>
-          <Text color={'gray.500'} noOfLines={2}>
-            In this post, we will give an overview of what is new in React 18,
-            and what it means for the future.
-          </Text>
         </Box>
         <HStack borderTop={'1px'} color="black">
           <Flex
@@ -83,7 +49,7 @@ function PokemonCard({ pokemon }: any) {
             roundedBottom={'sm'}
             borderLeft={'1px'}
             cursor="pointer"
-            onClick={() => setLiked(!liked)}
+            onClick={setLiked}
           >
             {liked ? (
               <BsHeartFill fill="red" fontSize={'24px'} />
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,8 +1,14 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Container, HStack, SimpleGrid, VStack } from '@chakra-ui/react';
 import PokemonCard from '@components/PokemonCard';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, fetchPokemons, RootState } from '../../store';
+import {
+  AppDispatch,
+  fetchPokemons,
+  PokemonProps,
+  RootState,
+} from '../../store';
 
 function List() {
   const { pokemons, loading } = useSelector(
@@ -10,11 +16,22 @@ function List() {
   );
   const dispatch = useDispatch<AppDispatch>();
 
-  console.log(pokemons);
+  const [favourites, setFavourites] = useLocalStorage<PokemonProps[]>(
+    'favorites',
+    []
+  );
 
   useEffect(() => {
     dispatch(fetchPokemons());
   }, []);
+
+  const handleFavourites = (pokemon: PokemonProps) => {
+    if (favourites.some((favourite) => favourite.name === pokemon.name)) {
+      setFavourites(favourites.filter((item) => item.name !== pokemon.name));
+    } else {
+      setFavourites([...favourites, pokemon]);
+    }
+  };
 
   if (loading) {
     return <Box>Loading...</Box>;
@@ -22,9 +39,22 @@ function List() {
 
   return (
     <Box>
-      {pokemons.map((pokemon) => (
-        <PokemonCard key={pokemon.url} pokemon={pokemon} />
-      ))}
+      <Container display="flex" flexDir="column" maxW={'6xl'} mt={10}>
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={10}>
+          {pokemons.map((pokemon) => (
+            <VStack key={pokemon.url} align={'top'}>
+              <PokemonCard
+                key={pokemon.url}
+                pokemon={pokemon}
+                liked={favourites.some(
+                  (favourite) => favourite.name === pokemon.name
+                )}
+                setLiked={() => handleFavourites(pokemon)}
+              />
+            </VStack>
+          ))}
+        </SimpleGrid>
+      </Container>
     </Box>
   );
 }
